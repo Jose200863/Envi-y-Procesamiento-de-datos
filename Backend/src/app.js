cb47207c8b2b2040cb7690beb0caff2a81913f5d
Jose200863 <brayanjoseazoefiaamdor02@gmail.com>
2026-06-16 09:42:47 -0600

    Se agregan modificaciones en package.json y se agregan constantes en app para que muestre nombre, version, y descripcion

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const NAME = process.env.SERVER_NAME;
+const VERSION = process.env.SERVER_VERSION;
+const DESCRIPTION = process.env.SERVER_DESCRIPTION;
+const PORT = process.env.SERVER_PORT || 4000;
 
 const app = express();
-const PORT = 4000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Servidor funcionando</h1>");
+  res.send(`
+    <h1>${NAME}</h1>
+    <p><strong>Version:</strong> ${VERSION}</p>
+    <p><strong>Description:</strong> ${DESCRIPTION}</p>
+  `);
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`${NAME} version ${VERSION} is running on http://localhost:${PORT}`);
 });
